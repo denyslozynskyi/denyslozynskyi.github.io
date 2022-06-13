c28aefcd808cc4dd20f20d50c936056a74118cc2
Denys Lozynskyi <denislozynskyi@gmail.com>
2022-06-13 17:02:37 +0300

    change file structure, section 4 - tabs

---
+++ pulse_step/js/script.js
@@ -0,0 +1,49 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    speed: 1200,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    touch: true,
+    autoplayButton: false,
+    autoplayButtonOutput: false,
+    responsive: {
+        769: {
+            autoplay: false,
+        },
+        300: {
+            autoplay: true,
+        }
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    slider.goTo('next');
+});
+
+$(document).ready(function(){
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
