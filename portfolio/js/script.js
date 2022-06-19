2795811a1d0e76cad48676b6985ea24cf43b8699
Denys Lozynskyi <denislozynskyi@gmail.com>
2022-06-20 00:16:33 +0300

    all sections done

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const menu = document.querySelector('.menu');
     const menuClose = document.querySelector('.menu__close');
+    const progressItems = Array.from(document.querySelectorAll('.progress-item'));
 
     hamburger.addEventListener('click', () => {
         menu.classList.add('active');
@@ -9,5 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     menuClose.addEventListener('click', () => {
         menu.classList.remove('active');
+    });
+
+    progressItems.forEach( (item) => {
+        let percents = item.querySelector('.progress__percents').innerHTML;
+        let progressLine = item.querySelector('.progress-item__progress');
+        
+        progressLine.style.width = percents;
     });
 });
